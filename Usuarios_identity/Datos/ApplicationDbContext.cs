916196a151cd3deb85b76dccae7d4b2ac6eedfb1
Carlos Perez <cps172779@gmail.com>
2022-09-05 20:12:03 -0500

    Primer commit

---
+++ Usuarios_identity/Datos/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Usuarios_identity.Datos
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
