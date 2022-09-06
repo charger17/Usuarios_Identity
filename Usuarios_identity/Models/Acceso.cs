17f252925589281e3edf4cbfadad5fa76ee47348
Carlos Perez <cps172779@gmail.com>
2022-09-05 20:33:29 -0500

    Se agrega el modelo de acceso

---
+++ Usuarios_identity/Models/Acceso.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Usuarios_identity.Models
+{
+    [NotMapped]
+    public class Acceso
+    {
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Display(Name ="¿Recordar datos?")]
+        public bool RememberMe { get; set; }
+    }
+}
